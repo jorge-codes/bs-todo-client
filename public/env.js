87a3d9990f09d49fa05646772ed5a17eee9e57b5
Jorge Palacios <reefaktor@gmail.com>
2021-02-17 05:55:58 -0300

    Added User components
    * Added User-related components from basic template done in bootstrap
    * Added Axios library

---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "OLI": "oli"
+};
---
+++ src/components/UserItem.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+class UserItem extends React.Component {
+
+  render() {
+    return(
+      <tr className="align-middle">
+        <td>
+          <span className="align-middle">Jessy Rocket</span>
+        </td>
+        <td className="mx-auto">
+          <div className="text-center">
+            <div className="btn-group btn-group-sm">
+              <button className="btn btn-outline-secondary"><i className="fas fa-edit"></i></button>
+              <button className="btn btn-primary"><i className="fas fa-tasks"></i></button>
+            </div>
+          </div>
+        </td>
+        <td>
+          <button className="btn btn-sm btn-danger"><i className="fas fa-trash"></i></button>
+        </td>
+      </tr>
+
+      // <tr>
+      //   <td>
+      //     <div className="input-group input-group-sm mb3">
+      //       <input type="text" className="form-control" value="Jorge Palacios"/>
+      //       <button className="btn btn-outline-secondary"><i class="far fa-times-circle"></i></button>
+      //       <button className="btn btn-success"><i class="fas fa-check"></i></button>
+      //     </div>
+      //   </td>
+      //   <td className="mx-auto">
+      //     <div className="text-center">
+      //       <div className="btn-group btn-group-sm invisible">
+      //         <button className="btn btn-outline-primary"><i class="fas fa-edit"></i></button>
+      //         <button className="btn btn-danger"><i class="fas fa-trash"></i></button>
+      //       </div>
+      //     </div>
+      //   </td>
+      //   <td>
+      //     <button className="btn btn-sm btn-danger invisible"><i class="fas fa-trash"></i></button>
+      //   </td>
+      // </tr>
+
+
+      // <tr className="align-middle">
+      //   <td>
+      //     <span className="align-middle">Meowth Rocket</span>
+      //   </td>
+      //   <td className="mx-auto">
+      //     <div className="text-center">
+      //       <div className="btn-group btn-group-sm">
+      //         <button className="btn btn-outline-secondary"><i class="fas fa-edit"></i></button>
+      //         <button className="btn btn-primary"><i class="fas fa-tasks"></i></button>
+      //       </div>
+      //     </div>
+      //   </td>
+      //   <td>
+      //     <button className="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+      //   </td>
+      // </tr>
+
+      // <tr className="align-middle">
+      //   <td>
+      //     <span className="align-middle">Boss Rocket</span>
+      //   </td>
+      //   <td className="mx-auto">
+      //     <div className="text-center">
+      //       <div className="btn-group btn-group-sm">
+      //         <button className="btn btn-outline-secondary"><i class="fas fa-edit"></i></button>
+      //         <button className="btn btn-primary"><i class="fas fa-tasks"></i></button>
+      //       </div>
+      //     </div>
+      //   </td>
+      //   <td>
+      //     <button className="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+      //   </td>
+      // </tr>
+    );
+  }
+}
+
+export default UserItem;
