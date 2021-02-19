e4b2ca7ac834c8849e80a00c507c8a8ccef72968
Jorge Palacios <reefaktor@gmail.com>
2021-02-19 09:29:45 -0300

    Implemented adding user
    * Implemented adding user
    * Removed unnecessary logs

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,41 +1,41 @@
 // components/UserForm.js
-
 import React from 'react';
 
-
 class UserForm extends React.Component {
+  nameRef = React.createRef();
 
-  addNewUser = (event) => {
+  addUser = (event) => {
     event.preventDefault();
-  }
+    const name = this.nameRef.current.value.trim();
+    this.props.addUser(name);
+    event.currentTarget.reset();
+  };
 
   render() {
-      return (
-        <section className="container">
-          <form>
-            <label htmlFor="input-user-name" className="form-label">
-              Add new user
-            </label>
-            <div className="input-group mb-4">
-              <input
-                autoFocus
-                type="text"
-                className="form-control"
-                id="input-user-name"
-                placeholder="Example: Jessy Rocket"
-              />
-              <button
-                type="submit"
-                id="btn-user-add"
-                className="btn btn-success"
-                onClick={this.addNewUser}
-              >
-                Add
-              </button>
-            </div>
-          </form>
-        </section>
-      );
+    return (
+      <section className='container'>
+        <form onSubmit={this.addUser}>
+          <label htmlFor='input-user-name' className='form-label'>
+            Add new user
+          </label>
+          <div className='input-group mb-4'>
+            <input
+              autoFocus
+              type='text'
+              name='name'
+              ref={this.nameRef}
+              className='form-control'
+              id='input-user-name'
+              placeholder='Example: Jessy Rocket'
+              size='250'
+            />
+            <button type='submit' id='btn-user-add' className='btn btn-success'>
+              Add
+            </button>
+          </div>
+        </form>
+      </section>
+    );
   }
 }
 
